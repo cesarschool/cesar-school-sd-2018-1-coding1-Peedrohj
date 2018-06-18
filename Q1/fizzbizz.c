f3ed0ef0907cb417bee045e3cdc52cf3c5c408f2
Peedrohj <38144264+Peedrohj@users.noreply.github.com>
2018-06-18 17:04:53 -0300

    Update fizzbizz.c

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -7,23 +7,35 @@
  * Output: utilize a função printf da biblioteca stdio.h e escreva um número ou palavra por linha (\n):
  * 
  */
-void fizzBizz(int start, int end)
-{
+#include <stdio.h>
+
+void fizzBuzz(int start, int end){
+    int cont = 1 ;
     printf("\nFizzBizz::START: %d, %d\n", start, end);
-    
-    // TODO: Your code here!
-    // Ex.: printf("Fizz\n");
-    
-    printf("\nFizzBizz::END\n");
+    cont = start;
+    while (cont < end){
+        if ((cont % 3) == 0 && (cont % 5) == 0){
+            printf("FizzBuzz\n");
+        }else if ((cont % 3) == 0){
+            printf("Fizz\n");
+        }else if ((cont % 5) ==  0){
+            printf("Buzz\n");
+        }else{
+            printf("%d\n", cont);
+        }
+    cont += 1;
+        
+    }
+
+    printf("\nFizzBuzz::END\n");
 }
 
-int main()
-{
+int main(){
     printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: FizzBizz ###\n");
     
-    fizzBizz(1, 6);
-    fizzBizz(1, 16);
-    fizzBizz(1, 100);
+    fizzBuzz(1, 6);
+    fizzBuzz(1, 16);
+    fizzBuzz(1, 100);
     
     return 0;
 }
