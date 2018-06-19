cd3cee87b5280a0699881ba197008cf303c726ea
Peedrohj <38144264+Peedrohj@users.noreply.github.com>
2018-06-19 00:00:03 -0300

    Update fnrc.c

--- Q3/fnrc.c
+++ Q3/fnrc.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-/**
- * Escreva uma função que encontra a primeira letra que não se repete em uma
- * sentença.
- * Função: char firstNonRepeatingChar(char[] sentence, int length)
- * Input:
- * - sentence: array não nulo de letras, formado apenas por ASCII em minúsculo.
- * - length: tamanho do array - número de caracteres
- * Output: A primeira letra que não se repete, ou -1, caso todas se repitam.
- */
+
+
 char firstNonRepeatingChar (char sentence[], int length)
 {
-    printf("\nfirstNonRepeatingChar::START: %s, %d\n", s, length);
+    printf("\nfirstNonRepeatingChar::START: %s, %d\n", sentence, length);
     
-    // TODO: YOUR CODE HERE!
+    int repete = 0;
+    int i;
+    char letra = sentence[i];
+
+    for (i = 0 ; i <= (length - 1); i++){
+        if(sentence[i] == ' '){
+            i++;
+            break;
+        }        
+        for(int x = 0; x <= ( length - 1); x++){
+            if(letra == sentence[j]){
+                repete++;
+        }        
+    }
+
+    if(repete ==1 ){
+        printf("%c", letra);
+    }
     
     printf("\nfirstNonRepeatingChar::END\n");
     return -1;
 }
-
 int main()
 {
     printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: firstNonRepeatingChar ###\n");
